d3c70096a18a412a15c4da63c1b233f6de2d625b
Adam Rutter <adamrutter92@gmail.com>
2020-12-14 16:22:15 +0000

    Add notifications context

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,9 @@
 import { ChakraProvider, theme } from "@chakra-ui/react"
+import { NotificationsProvider } from "../contexts/NotificationsContext"
 import React from "react"
 
-export const App = () => <ChakraProvider theme={theme}></ChakraProvider>
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <NotificationsProvider></NotificationsProvider>
+  </ChakraProvider>
+)
