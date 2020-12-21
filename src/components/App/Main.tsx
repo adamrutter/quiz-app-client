a406e5c4495c920442d3d6a70414f6e672a1fe74
Adam Rutter <adamrutter92@gmail.com>
2020-12-21 01:24:21 +0000

    Tab/panel alignment

--- src/components/App/Main.tsx
+++ src/components/App/Main.tsx
@@ -1,7 +1,32 @@
-import { Tab, Tabs, TabList, TabPanel, TabPanels } from "@chakra-ui/react"
+import {
+  Container,
+  Tab as ChakraTab,
+  Tabs,
+  TabList,
+  TabPanel as ChakraTabPanel,
+  TabPanelProps,
+  TabPanels,
+  TabProps
+} from "@chakra-ui/react"
 import { Party } from "./Main/Party"
 import { Quiz } from "./Main/Quiz"
 import React from "react"
+
+const Tab = ({ children, ...rest }: TabProps) => {
+  return (
+    <ChakraTab px={5} {...rest}>
+      {children}
+    </ChakraTab>
+  )
+}
+
+const TabPanel = ({ children, ...rest }: TabPanelProps) => {
+  return (
+    <ChakraTabPanel my={6} {...rest}>
+      <Container>{children}</Container>
+    </ChakraTabPanel>
+  )
+}
 
 export const Main = () => {
   return (
