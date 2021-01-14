eaee3ddae00761572442588616f655e6126d34de
Adam Rutter <adamrutter92@gmail.com>
2021-01-14 13:10:22 +0000

    Party tab

--- src/components/App/Main/Party.tsx
+++ src/components/App/Main/Party.tsx
@@ -1,5 +1,16 @@
+import { ChangeName } from "./Party/ChangeName"
+import { Divider } from "@chakra-ui/react"
+import { InviteFriends } from "./Party/InviteFriends"
+import { PartyMembers } from "./Party/PartyMembers"
 import React from "react"
 
 export const Party = () => {
-  return <>Party</>
+  return (
+    <>
+      <InviteFriends />
+      <Divider my={7} />
+      <PartyMembers />
+      <ChangeName />
+    </>
+  )
 }
