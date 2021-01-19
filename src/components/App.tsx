066ad546caa3ff5d872be3d929cb4a593297d2f6
Adam Rutter <adamrutter92@gmail.com>
2021-01-19 00:44:22 +0000

    Remove unused notifications context

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom"
 import { ChakraProvider } from "@chakra-ui/react"
 import { DisplayNameProvider } from "../contexts/DisplayNameContext"
-import { NotificationsProvider } from "../contexts/NotificationsContext"
 import { PartyProvider } from "../contexts/PartyContext"
 import { QuizProvider } from "../contexts/QuizContext"
 import { SocketIOProvider } from "../contexts/SocketIOContext"
@@ -15,22 +14,20 @@ import { PartyMembersProvider } from "contexts/PartyMembersContext"
 export const App = () => (
   <Router>
     <ChakraProvider theme={theme}>
-      <NotificationsProvider>
-        <SocketIOProvider>
-          <UserProvider>
-            <PartyProvider>
-              <QuizProvider>
-                <DisplayNameProvider>
-                  <PartyMembersProvider>
-                    <Header />
-                    <Main />
-                  </PartyMembersProvider>
-                </DisplayNameProvider>
-              </QuizProvider>
-            </PartyProvider>
-          </UserProvider>
-        </SocketIOProvider>
-      </NotificationsProvider>
+      <SocketIOProvider>
+        <UserProvider>
+          <PartyProvider>
+            <QuizProvider>
+              <DisplayNameProvider>
+                <PartyMembersProvider>
+                  <Header />
+                  <Main />
+                </PartyMembersProvider>
+              </DisplayNameProvider>
+            </QuizProvider>
+          </PartyProvider>
+        </UserProvider>
+      </SocketIOProvider>
     </ChakraProvider>
   </Router>
 )
