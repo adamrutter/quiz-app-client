b2e8d4fae5db692bc04fe905ba6c1417c860aa7f
Adam Rutter <adamrutter92@gmail.com>
2021-01-19 01:37:34 +0000

    Add an alert for no party leader

--- src/components/App/Main/Quiz.tsx
+++ src/components/App/Main/Quiz.tsx
@@ -1,6 +1,7 @@
-import { Box, Spinner } from "@chakra-ui/react"
+import { Alert, AlertIcon, Box, Spinner } from "@chakra-ui/react"
 import { Game } from "./Quiz/Game"
 import { OptionsForm } from "./Quiz/OptionsForm"
+import { PartyMembers as PartyMembersContext } from "contexts/PartyMembersContext"
 import { Quiz as QuizId } from "contexts/QuizContext"
 import { ReadyPrompt } from "./Quiz/ReadyPrompt"
 import { SocketIO } from "contexts/SocketIOContext"
@@ -13,6 +14,7 @@ export const Quiz = () => {
   const socket = useContext(SocketIO)
   const quizId = useContext(QuizId)
   const userId = useContext(User)
+  const partyMembers = useContext(PartyMembersContext)
   const partyLeader = usePartyLeader()
 
   // Whether to show the pre-quiz ready prompt
@@ -30,19 +32,24 @@ export const Quiz = () => {
     }
   }, [])
 
+  const isLoading = partyMembers.length === 0
+
   return (
     <>
       <ReadyPrompt isOpen={readyPromptOpen} />
-      {partyLeader ? (
-        userId === partyLeader?.id ? (
-          <OptionsForm />
-        ) : (
-          <WaitForStart />
-        )
-      ) : (
+      {isLoading && (
         <Box>
           Loading... <Spinner ml={2} size="sm" speed="1s" />
         </Box>
+      )}
+      {!isLoading &&
+        partyLeader &&
+        (userId === partyLeader?.id ? <OptionsForm /> : <WaitForStart />)}
+      {!isLoading && !partyLeader && (
+        <Alert my={3} status="error" textAlign="left" variant="left-accent">
+          <AlertIcon />
+          There is no party leader. Please either create or join another party.
+        </Alert>
       )}
       {quizId && <Game />}
     </>
