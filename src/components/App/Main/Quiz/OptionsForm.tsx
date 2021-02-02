323d4df3b6fbaccfc72d3681039d27723616e3b0
Adam Rutter <adamrutter92@gmail.com>
2021-02-02 16:19:20 +0000

    Correct useEffect dependencies

--- src/components/App/Main/Quiz/OptionsForm.tsx
+++ src/components/App/Main/Quiz/OptionsForm.tsx
@@ -113,7 +113,7 @@ export const OptionsForm = () => {
   // Send human formatted options to socket.io so they can be emitted to all clients
   useEffect(() => {
     socket.emit("party-leader-quiz-options", humanOptions, partyId)
-  }, [humanOptions])
+  }, [humanOptions, partyId, socket])
 
   // Set up the radio button cards
   // See https://chakra-ui.com/docs/form/radio#custom-radio-buttons
